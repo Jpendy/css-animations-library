813ac1703bdb02487f8b50a0bae0a637e0a3513c
Jpendy <Jpendy256@gmail.com>
2020-10-18 16:12:26 -0700

    updating styles

--- src/data/grid-classes.js
+++ src/data/grid-classes.js
@@ -14,34 +14,34 @@ export default [
             <div class="grid-1-box-4"> </div>
         </div>
         `,
-        css: `.grid-1-container{
-            width: 1000px;
-            height: 1000px;
+        css: `
+        .grid-1-container{
+            height: 800px;
+            width: 1400px;
+            background: lightcyan;
+            padding: 40px;
             display: grid;
             justify-content: center;
             grid-template-columns: repeat(3, 1fr);
-            grid-gap: 20px;
+            grid-gap: 30px;
         }
-        
         .grid-1-box-1 {
             grid-column: 1/2;
             background: blue;
         }
-        
         .grid-1-box-2 {
             grid-column: 2/4;
             background: green;
         }
-        
         .grid-1-box-3 {
             grid-column: 1/3;
             background: yellow;
         }
-        
         .grid-1-box-4 {
             grid-column: 3/4;
             background: red;
-        }`
+        }
+        `
     },
     {
         className: 'Grid-2',
@@ -55,9 +55,12 @@ export default [
             <div class="grid-2-box-6"> </div>
         </div>
         `,
-        css: `.grid-2-container {
-            height: 100%;
-            width: 100%;
+        css: `
+        .grid-2-container {
+            height: 800px;
+            width: 1400px;
+            background: lightcyan;
+            padding: 40px;
             display: grid;
             grid-template-columns: repeat(8, 1fr);
             grid-template-areas: 
@@ -65,9 +68,8 @@ export default [
             "leftSide leftSide upperMid upperMid upperMid upperMid rightSide rightSide"
             "leftSide leftSide lowerMid lowerMid lowerMid lowerMid rightSide rightSide"
             "footer footer footer footer footer footer footer footer";
-            grid-gap: 30px;
+            grid-gap: 20px;
         }
-        
         .grid-2-box-1 {
             grid-area: header;
             background: orange;
@@ -91,6 +93,7 @@ export default [
         .grid-2-box-6 {
             grid-area: footer;
             background: lightseagreen;
-        }`
+        }
+        `
     },
 ]
