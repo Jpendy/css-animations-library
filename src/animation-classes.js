5cce23dbc99576e00d921c2d287f90e2e61ddd30
Jpendy <Jpendy256@gmail.com>
2020-10-18 02:13:59 -0700

    starting adding grids

--- src/animation-classes.js
+++ src/animation-classes.js
@@ -1,19 +1,142 @@
 export default [
     {
         className: 'original',
-        code: `code code code`
+        css: `code code code`
     },
     {
-        className: 'red-background',
-        code: ``
+        className: 'grid-1',
+        html: `
+        <div class="grid-1-container" >
+            <div class="grid-1-box-1"> </div>
+            <div class="grid-1-box-2"> </div>
+            <div class="grid-1-box-3"> </div>
+            <div class="grid-1-box-4"> </div>
+        </div>
+        `,
+        css: `.grid-1-container{
+            width: 1000px;
+            height: 1000px;
+            display: grid;
+            justify-content: center;
+            grid-template-columns: repeat(3, 1fr);
+            grid-gap: 20px;
+        }
+        
+        .grid-1-box-1 {
+            grid-column: 1/2;
+            background: blue;
+        }
+        
+        .grid-1-box-2 {
+            grid-column: 2/4;
+            background: green;
+        }
+        
+        .grid-1-box-3 {
+            grid-column: 1/3;
+            background: yellow;
+        }
+        
+        .grid-1-box-4 {
+            grid-column: 3/4;
+            background: red;
+        }`
+    },
+    {
+        className: 'grid-2',
+        html: `
+        <div class="grid-2-container" >
+            <div class="grid-2-box-1"> </div>
+            <div class="grid-2-box-2"> </div>
+            <div class="grid-2-box-3"> </div>
+            <div class="grid-2-box-4"> </div>
+            <div class="grid-2-box-5"> </div>
+            <div class="grid-2-box-6"> </div>
+        </div>
+        `,
+        css: `.grid-2-container {
+            height: 100%;
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(8, 1fr);
+            grid-template-areas: 
+            "header header header header header header header header"
+            "leftSide upperMid upperMid upperMid upperMid upperMid upperMid rightSide"
+            "leftSide lowerMid lowerMid lowerMid lowerMid lowerMid lowerMid rightSide"
+            "footer footer footer footer footer footer footer footer";
+            grid-gap: 20px;
+        }
+        
+        .grid-2-box-1 {
+            grid-area: header;
+            background: orange;
+        }
+        .grid-2-box-2 {
+            grid-area: leftSide;
+            background: green;
+        }
+        .grid-2-box-3 {
+            grid-area: rightSide;
+            background: red;
+        }
+        .grid-2-box-4 {
+            grid-area: upperMid;
+            background: orchid;
+        }
+        .grid-2-box-5 {
+            grid-area: lowerMid;
+            background: lime;
+        }
+        .grid-2-box-6 {
+            grid-area: footer;
+            background: lightseagreen;
+        }`
+    },
+    {
+        className: 'divs',
+        html: `<div class="div-1 thing" >1</div>
+               <div class="div-2 thing" >2</div>
+               <div class="div-3 thing" >3</div>
+               <div class="div-4 thing" >4</div>`,
+        css: `code code code`
+    },
+    {
+        className: 'wobble',
+        css: `.wobble {
+            animation: wobble 2s linear infinite;
+        }
+
+        @keyframes wobble {
+            20% {
+            transform: translateX(-50px);
+            transform: scale(1.2)
+            }
+            40% {
+                transform: translateY(-50px);
+                transform: scale(.4);
+            }
+            55% {
+                transform: translateX(200px);
+            } 
+            70% {
+                transform: scale(.4);
+            }
+            80% {
+                transform: translateY(-50px);
+        
+            }
+        }`
     },
     {
         className: 'blue-background',
-        code: ``
+        css: ``
     },
     {
         className: 'radiate',
-        code: `.radiate {
+        html: `
+        <div class="radiate main-element">1</div>
+        `,
+        css: `.radiate {
             animation: radiate 1.5s infinite ease-in-out;
         }
 
@@ -37,9 +160,9 @@ export default [
     },
     {
         className: 'spin',
-        code: `.spin {
+        css: `.spin {
             animation: spin 20s infinite linear;
-            
+
             @keyframes spin {
                 from {
                   transform: rotate(0deg);
--- src/containers/MainContainer/MainContainer.jsx
+++ src/containers/MainContainer/MainContainer.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import reactHtmlParser from 'react-html-parser'
 import SelectClass from '../../components/selectClass/SelectClass'
 import animationClasses from '../../animation-classes.js'
 import './MainContainer.css'
@@ -15,21 +16,28 @@ export default function MainContainer() {
     return (
         <>
             <main id="main">
-
+                <div className="select-div" >
+                    <SelectClass handleChange={handleSelectOptionChange} />
+                </div>
                 <section className="top-section" >
 
-                    <div className="select-div" >
-                        <SelectClass handleChange={handleSelectOptionChange} />
-                    </div>
-                    <div id="main-element" className={`main-element original-background-color ${cssClass}`} >
-                        MAIN ELEMENT
+                    <div className="elements-div" >
+                        {reactHtmlParser(animationClasses.find(item => item.className === cssClass).html)}
                     </div>
 
                 </section>
 
-                <section className="code-section">
+                <section className="html-code-section" >
                     <pre>
-                        {animationClasses.find(item => item.className === cssClass).code}
+                        <h2>HTML</h2>
+                        {animationClasses.find(item => item.className === cssClass).html}
+                    </pre>
+                </section>
+
+                <section className="css-code-section">
+                    <pre>
+                        <h2>CSS</h2>
+                        {animationClasses.find(item => item.className === cssClass).css}
                     </pre>
                 </section>
 
